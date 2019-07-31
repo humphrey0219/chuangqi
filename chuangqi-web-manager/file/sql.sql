24a863dbb0f8742aaf125afc8764618caefb18d3
wmk123654 <347948093@qq.com>
2019-08-01 00:16:56 +0800

    新增账户sql

---
+++ chuangqi-web-manager/file/sql.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `t_sys_account` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主见',
+  `userName` varchar(50) NOT NULL COMMENT '用户名',
+  `pwd` varchar(64) NOT NULL COMMENT '密码',
+  `realName` varchar(64) DEFAULT NULL COMMENT '真实姓名',
+  `status` tinyint(2) DEFAULT '1' COMMENT '状态：1启用，0禁用',
+  `createTime` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `updateTime` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uq_userName` (`userName`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='系统账号';
